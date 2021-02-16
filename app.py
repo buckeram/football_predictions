2e724565d905986c6d5be1a7e38b964dab484b5f
Alan Buckeridge <alan.buckeridge@amobee.com>
2021-02-16 23:56:17 +0000

    Added lambda to download historical data to S3 hourly
    By the way, changed get_fixtures to retrieve the oddsportal data
    from a Google Cloud Function I created because oddsportal doesn't
    block requests from there. Feels a bit hacky but it's working so far.

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 import boto3
+import botocore
 import logging
-import datetime
-import proxyscrape
+import os
 import re
+import requests
 import tablib
-from chalice import Chalice, Rate
-from requests_html import HTMLSession
+from chalice import Chalice
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.util.retry import Retry
+from chalicelib import common
+from datetime import datetime, timezone
 
 
 app = Chalice(app_name='football_predictions')
@@ -15,101 +19,157 @@ LOGGER.setLevel(logging.INFO)
 
 s3 = boto3.resource('s3')
 
-fieldnames = ['Country', 'League', 'Date', 'Time', 'HomeTeam', 'AwayTeam', 'AvgH', 'AvgD', 'AvgA']
-num_days = 2 # How many days worth of matches to retrieve
-
-session = HTMLSession()
-collector = proxyscrape.create_collector('fixtures-proxy', ['http', 'https'])
+num_days = int(os.environ['FIXTURES_DAYS'])
 
 
-@app.schedule(Rate(10, unit=Rate.MINUTES))
+@app.schedule('cron(0 18 * * ? *)')
 def get_fixtures(event):
 
-    collector = proxyscrape.get_collector('fixtures-proxy')
-    http_proxy = collector.get_proxy({'type': 'http'})
-    https_proxy = collector.get_proxy({'type': 'https'})
-    LOGGER.info("HTTP Proxy: %s", http_proxy)
-    LOGGER.info("HTTPS Proxy: %s", https_proxy)
-    proxies = {
-        'http': "http://{}:{}".format(http_proxy.host, http_proxy.port),
-        'https': "http://{}:{}".format(https_proxy.host, https_proxy.port)
-    }
+    # Set up the requests Session with retries
+    s = requests.Session()
+    retries = Retry(total=5, backoff_factor=1, status_forcelist=[ 500, 502, 503, 504 ])
+    s.mount('http://', HTTPAdapter(max_retries=retries))
+    s.mount('https://', HTTPAdapter(max_retries=retries))
 
+    # Retrieve the fixtures from the web api
+    url = os.environ['FIXTURES_URL']
+    LOGGER.info("Fetching fixtures from %s", url)
+    r = s.get(url, verify=False, timeout=240)
+
+    # Load the csv into a tablib Dataset
     data = tablib.Dataset()
-    data.headers = fieldnames
+    data.csv = r.text
 
-    for i in range(num_days):
+    # Add column which represents the 'football-data' division
+    data.lpush_col(_get_division, header='Div')
+    del data['Country']
+    del data['League']
 
-        day = datetime.date.today() + datetime.timedelta(days=i+1)
-        url = "https://www.oddsportal.com/matches/soccer/%s/" % day.strftime("%Y%m%d")
-        LOGGER.info("Fetching fixtures from %s", url)
-
-        r = session.get(url, proxies=proxies, timeout=60, verify=False)
-        r.html.render(wait=5.0, sleep=20, retries=10, timeout=120)
-
-        table_matches = r.html.find('div#table-matches > table.table-main', first=True)
-        league = ""
-        for tr in table_matches.find('tr'):
-            if 'class' in tr.attrs:
-                css_class = tr.attrs['class']
-                if css_class:
-                    if 'dark' in css_class:
-                        country, division = tr.find('th.tl', first=True).text.split('»')
-                    elif 'odd' in css_class:
-                        fixture = tr.text.replace('postp.', '')
-                        fixture = re.sub(r'\n+', '\t', fixture)
-                        time, match, home_us, draw_us, away_us = fixture.split('\t')[:5]
-                        match_date = day.strftime("%d/%m/%Y")
-                        home_eu = us_odds_to_decimal(home_us)
-                        draw_eu = us_odds_to_decimal(draw_us)
-                        away_eu = us_odds_to_decimal(away_us)
-                        home_team, away_team = match.split(' - ')
-
-                        data.append([country, division, match_date, time,
-                                     home_team.strip(), away_team.strip(),
-                                     home_eu, draw_eu, away_eu])
+    # Remove the rows for leagues we don't want (i.e. ones not in ODDSPORTAL_DIVISIONS)
+    keepers = [i for i, row in enumerate(data) if row[0] is not None]
+    subset = data.subset(rows=keepers)
 
     s3_bucket = os.environ['S3_BUCKET']
     s3_key = 'fixtures/fixtures.csv'
     LOGGER.info("Writing fixtures to %s/%s", s3_bucket, s3_key)
-    s3.Object(s3_bucket, s3_key).put(Body=data.export('csv'))
+    s3.Object(s3_bucket, s3_key).put(Body=subset.export('csv'))
+
+    LOGGER.info("Done.")
 
 
-def us_odds_to_decimal(odds_str):
+@app.schedule('rate(1 hour)')
+def fetch_historical_data(event):
 
-    if '.' in odds_str:
-        # Not US odds
-        return float(odds_str)
+    base_url_main = 'https://www.football-data.co.uk/mmz4281/'
+    base_url_new  = 'https://www.football-data.co.uk/new/'
+    latest_season = os.environ['LATEST_SEASON']
+    previous_seasons = os.environ['PREVIOUS_SEASONS']
+    main_leagues = os.environ['MAIN_LEAGUES']
+    new_leagues = os.environ['NEW_LEAGUES']
+    s3_bucket = os.environ['S3_BUCKET']
+    s3_prefix = os.environ['S3_PREFIX_HISTORICAL']
 
-    us_odds = int(odds_str)
-    eu_odds = 0.0
-    if us_odds > 0:
-        eu_odds = 1 + (us_odds / 100)
+    n_updates = 0
+
+    # Check each 'main' league for updates and overwrite if necessary
+    for league in [l.strip() for l in main_leagues.split(',')]:
+        n_updates += _update_historical_data(s3_bucket, s3_prefix, base_url_main, league,
+                                             latest_season=latest_season,
+                                             previous_seasons=previous_seasons)
+    # Check each 'new' league for updates and overwrite if necessary
+    for league in [l.strip() for l in new_leagues.split(',')]:
+        n_updates += _update_historical_data(s3_bucket, s3_prefix,
+                                             base_url_new, league)
+    return {
+        'statusCode': 200,
+        'body': "Number of updates: {}".format(n_updates)
+    }
+
+
+@app.on_s3_event(bucket=os.environ['S3_BUCKET'], prefix='fixtures/',
+                 suffix='.csv', events=['s3:ObjectCreated:*'])
+def make_predictions(event):
+    print("Looks like there's a new fixtures file: {}/{}".format(event.bucket, event.key))
+
+    # TODO complete
+
+
+def _update_historical_data(s3_bucket, s3_prefix, base_url, league, latest_season=None, previous_seasons=None):
+
+        # Check last-modified time of our copy of the file
+        s3object = _get_historical_data_from_s3(s3_bucket, s3_prefix, league)
+        our_last_modified = _get_last_modified(s3object)
+
+        # Download the data for the latest season
+        url = base_url
+        if latest_season:
+            url = base_url + latest_season
+        latest_data = _download_football_data(url, league)
+
+        # Compare last-modified dates; if latest_data is not newer, then nothing to do
+        # (Not sure if I'm doing the right thing here with UTC -- prob. should be BST)
+        their_last_modified = datetime.strptime(
+            latest_data.headers['Last-Modified'], "%a, %d %b %Y %H:%M:%S %Z").astimezone(timezone.utc)
+        LOGGER.info("%s last modified: %s", latest_data.url, their_last_modified)
+
+        if our_last_modified is not None and our_last_modified >= their_last_modified:
+            LOGGER.info("No changes for %s.", league)
+            return 0
+
+        # Get the previous seasons' data (for 'main' leagues only), combine it with latest season's data, and write to S3
+        data = ''
+        if previous_seasons:
+            for season in sorted([s.strip() for s in previous_seasons.split(',')]):
+                url = base_url + season
+                past_season = _download_football_data(url, league)
+                data += past_season.text
+        data += latest_data.text
+
+        LOGGER.info("Writing data to %s/%s/%s.csv", s3_bucket, s3_prefix, league)
+        s3object.put(Body=data)
+
+        return 1
+
+
+def _get_division(row):
+    country_and_league = row[0], row[1]
+    if country_and_league in common.ODDSPORTAL_DIVISIONS:
+        return common.ODDSPORTAL_DIVISIONS[country_and_league]
     else:
-        eu_odds = 1 - (100 / us_odds)
-
-    return eu_odds
+        return None
 
 
-# @app.route('/')
-# def index():
-#     return {'hello': 'world'}
-# The view function above will return {"hello": "world"}
-# whenever you make an HTTP GET request to '/'.
-#
-# Here are a few more examples:
-#
-# @app.route('/hello/{name}')
-# def hello_name(name):
-#    # '/hello/james' -> {"hello": "james"}
-#    return {'hello': name}
-#
-# @app.route('/users', methods=['POST'])
-# def create_user():
-#     # This is the JSON body the user sent in their POST request.
-#     user_as_json = app.current_request.json_body
-#     # We'll echo the json body back to the user in a 'user' key.
-#     return {'user': user_as_json}
-#
-# See the README documentation for more examples.
-#
+def _get_historical_data_from_s3(bucket, prefix, league):
+    key = prefix.rstrip('/') + '/' + league + '.csv'
+    return s3.Object(bucket, key)
+
+
+def _get_last_modified(s3object):
+    last_modified = None
+    try:
+        s3object.load()
+    except botocore.exceptions.ClientError as e:
+        if e.response['Error']['Code'] in ['403', '404']:
+            # The object does not exist.
+            LOGGER.info("%s/%s does not exist", s3object.bucket_name, s3object.key)
+        else:
+            # Something else has gone wrong.
+            raise
+    else:
+        # The object does exist
+        last_modified = s3object.last_modified
+
+    LOGGER.info("%s/%s last modified: %s" , s3object.bucket_name, s3object.key, last_modified)
+    return last_modified
+
+
+def _download_football_data(base_url, league):
+    # e.g https://www.football-data.co.uk/mmz4281/2021/E0.csv,
+    # or https://www.football-data.co.uk/new/AUT.csv
+    url = base_url.rstrip('/') + '/' + league + '.csv'
+    response = requests.get(url)
+    response.raise_for_status()
+    if response.status_code != 200:
+        raise Exception("Can't download from %s: status_code=%d, reason=%s" %
+                        (url, response.status_code, response.reason))
+    return response
---
+++ chalicelib/common.py
@@ -0,0 +1,34 @@
+# A mapping from oddsportal.com leagues to football-data.co.uk's 'Div'
+ODDSPORTAL_DIVISIONS = {
+    ('England', 'Premier League'): 'E0',
+    # ('England', 'Championship'): 'E1',
+    ('England', 'League One'): 'E2',
+    ('England', 'League Two'): 'E3',
+    ('England', 'National League'): 'EC',
+
+    ('Austria', 'Tipico Bundesliga'): 'AUT',
+    ('Belgium', 'Jupiler League'): 'B1',
+    ('Brazil', 'Serie A'): 'BRA',
+    ('Denmark', 'Superliga'): 'DNK',
+    ('France', 'Ligue 1'): 'F1',
+    ('France', 'Ligue 2'): 'F2',
+    ('Germany', 'Bundesliga'): 'D1',
+    ('Greece', 'Super League'): 'G1',
+    ('Ireland', 'Premier Division'): 'IRL',
+    ('Italy', 'Serie A'): 'I1',
+    ('Italy', 'Serie B'): 'I2',
+    ('Mexico', 'Liga MX'): 'MEX',
+    ('Netherlands', 'Eredivisie'): 'N1',
+    ('Norway', 'Eliteserien'): 'NOR',
+    ('Poland', 'Ekstraklasa'): 'POL',
+    ('Portugal', 'Primeira Liga'): 'P1',
+    ('Romania', 'Liga 1'): 'ROU',
+    ('Russia', 'Premier League'): 'RUS',
+    ('Scotland', 'Premiership'): 'SC0',
+    ('Spain', 'LaLiga'): 'SP1',
+    ('Sweden', 'Allsvenskan'): 'SWE',
+    ('Switzerland', 'Super League'): 'SWZ',
+    ('Turkey', 'Super Lig'): 'T1',
+    ('China', 'Super League'): 'CHN', # check
+    ('Japan', 'J1 League'): 'JPN' # check
+}

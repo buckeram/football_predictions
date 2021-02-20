4149e4081cb587e36f83c15b23e982d100211a4b
Alan Buckeridge <alan.buckeridge@amobee.com>
2021-02-20 22:59:58 +0000

    Send email with top predictions, and mail predicting draws

--- chalicelib/common.py
+++ chalicelib/common.py
@@ -1,7 +1,7 @@
 # A mapping from oddsportal.com leagues to football-data.co.uk's 'Div'
 ODDSPORTAL_DIVISIONS = {
     ('England', 'Premier League'): 'E0',
-    # ('England', 'Championship'): 'E1',
+    ('England', 'Championship'): 'E1',
     ('England', 'League One'): 'E2',
     ('England', 'League Two'): 'E3',
     ('England', 'National League'): 'EC',
@@ -33,11 +33,48 @@ ODDSPORTAL_DIVISIONS = {
     ('Japan', 'J1 League'): 'JPN' # check
 }
 
+DIVISIONS_TO_LEAGUES = {
+    'E0': 'England Premiership',
+    'E1': 'England Championship',
+    'E2': 'England League 1',
+    'E3': 'England League 2',
+    'EC': 'England National League',
+    'AUT': 'Austria Tipico Bundesliga',
+    'B1': 'Belgium Jupiler League',
+    'BRA': 'Brazil Serie A',
+    'DNK': 'Denmark Superligaen',
+    'D1': 'Germany Bundesliga',
+    'F1': 'France Ligue 1',
+    'F2': 'France Ligue 2',
+    'G1': 'Greece Super League 1',
+    'IRL': 'Ireland Premier Division',
+    'I1': 'Italy Serie A',
+    'I2': 'Italy Serie B',
+    'MEX': 'Mexico Clausura/Liga MX',
+    'N1': 'Netherlands Eredivisie',
+    'NOR': 'Norway Eliteserien',
+    'POL': 'Poland Ekstraklasa',
+    'P1': 'Portugal Primeira Liga',
+    'ROU': 'Romania Liga 1',
+    'RUS': 'Russia Premier League',
+    'SC0': 'Scotland Premiership',
+    'SP1': 'Spain LaLiga',
+    'SWE': 'Sweden Allsvenskan',
+    'SWZ': 'Switzerland Super League',
+    'T1': 'Turkey 1 Lig',
+    'CHN': 'China Super League',
+    'JPN': 'Japan J1 League'
+}
+
 # Mapping of oddsportal team names to football-data teams
 ODDSPORTAL_TEAMS = {
     'B1': {
         'KV Mechelen': 'Mechelen',
-        'Cercle Brugge KSV': 'Cercle Brugge'
+        'Cercle Brugge KSV': 'Cercle Brugge',
+        'St. Liege': 'Standard',
+        'St. Truiden': 'St Truiden',
+        'Club Brugge KV': 'Club Brugge',
+        'Leuven': 'Oud-Heverlee Leuven'
     },
     'E0': {
         'Manchester Utd': 'Man United',
@@ -71,34 +108,55 @@ ODDSPORTAL_TEAMS = {
         'Arminia Bielefeld': 'Bielefeld',
         'B. Monchengladbach': "M'gladbach",
         'Eintracht Frankfurt': 'Ein Frankfurt',
-        'Schalke': 'Schalke 04'
+        'Schalke': 'Schalke 04',
+        'Bayer Leverkusen': 'Leverkusen',
+        'Hertha Berlin': 'Hertha',
+
     },
     'G1': {
         'AEL Larissa': 'Larisa',
         'AEK Athens FC': 'AEK',
-        'Smyrnis': 'Apollon'
+        'Smyrnis': 'Apollon',
+        'Volos': 'Volos NFC',
+        'Olympiacos Piraeus': 'Olympiakos'
+    },
+    'I1': {
+        'AC Milan': 'Milan',
+        'AS Roma': 'Roma'
     },
     'N1': {
         'Venlo': 'VVV Venlo',
-        'Sittard': 'For Sittard'
+        'Sittard': 'For Sittard',
+        'PSV': 'PSV Eindhoven'
     },
     'P1': {
         'Sporting': 'Sp Lisbon',
         'Ferreira': 'Pacos Ferreira',
         'Vitoria Guimaraes': 'Guimaraes',
-        'SC Farense': 'Farense'
+        'SC Farense': 'Farense',
+        'Braga': 'Sp Braga',
+        'SC Farense': 'Farense',
+        'FC Porto': 'Porto'
     },
     'ROU': {
         'Univ. Craiova': 'U Craiova',
         'FCSB': 'FC Steaua Bucuresti'
     },
+    'SC0': {
+        'Dundee Utd': 'Dundee United',
+        'Dundee': 'Dundee United',
+        'St. Mirren': 'St Mirren'
+    },
     'SP1': {
         'Cadiz CF': 'Cadiz',
         'Atl. Madrid': 'Ath Madrid',
-        'Celta Vigo': 'Celta'
+        'Celta Vigo': 'Celta',
+        'Real Sociedad': 'Sociedad',
+        'Granada CF': 'Granada'
     },
     'T1': {
-        'Basaksehir': 'Buyuksehyr'
+        'Basaksehir': 'Buyuksehyr',
+        'Goztepe': 'Goztep'
     }
 }
 
--- chalicelib/predictions.py
+++ chalicelib/predictions.py
@@ -1,4 +1,5 @@
 import boto3
+import json
 import logging
 import numpy as np
 import os
@@ -60,7 +61,7 @@ def make_predictions(event):
     all_classifiers = dict()
     classifier_scores = dict()
 
-    mail_message = ""
+    all_predictions = []
 
     for league in leagues:
         LOGGER.info("League: %s", league)
@@ -73,8 +74,17 @@ def make_predictions(event):
             LOGGER.info("Hmm, no matches for league '%s'", league)
             continue
 
-        home_clf, draw_clf, away_clf, best_home_score, best_draw_score, best_away_score, home_best_fname, draw_best_fname, away_best_fname, train_summary = train_classifiers(past_league_matches, league)
-        mail_message += train_summary
+        league_predictions = dict()
+        league_predictions['div'] = league
+        league_predictions['league'] = common.DIVISIONS_TO_LEAGUES[league]
+
+        home_clf, draw_clf, away_clf, best_home_score, best_draw_score, best_away_score, home_best_fname, draw_best_fname, away_best_fname = train_classifiers(past_league_matches, league)
+        league_predictions['home_score'] = best_home_score
+        league_predictions['draw_score'] = best_draw_score
+        league_predictions['away_score'] = best_away_score
+        league_predictions['home_fname'] = home_best_fname
+        league_predictions['draw_fname'] = draw_best_fname
+        league_predictions['away_fname'] = away_best_fname
 
         classifiers = {'home_clf': home_clf, 'draw_clf': draw_clf, 'away_clf': away_clf,
             'home_fname': home_best_fname, 'draw_fname': draw_best_fname, 'away_fname': away_best_fname}
@@ -83,10 +93,140 @@ def make_predictions(event):
         next_league_fixtures = get_league_data(fixtures, league)
         next_matches = zip(next_league_fixtures['HomeTeam'], next_league_fixtures['AwayTeam'])
 
-        mail_message += predict(past_league_matches, league, classifiers, next_matches)
+        predictions = predict(past_league_matches, league, classifiers, next_matches)
+        league_predictions['predictions'] = predictions
+        all_predictions.append(league_predictions)
+
+    predictions_json = json.dumps(all_predictions)
+    email_all_predictions(predictions_json)
+    email_best_predictions(predictions_json)
+    email_draw_predictions(predictions_json)
 
 
-    LOGGER.info("Sending mail...")
+def email_all_predictions(all_predictions_json):
+    league_template = textwrap.dedent("""
+    ==================================================
+    %s: %s
+    ==================================================
+    Home (%s) %.2f
+    Away (%s) %.2f
+    Draw (%s) %.2f
+
+    """)
+    match_template = textwrap.dedent("""
+    %s v. %s
+    --------------------------------------------------
+    Home: %.2f   (%.2f)
+    Draw: %.2f   (%.2f)
+    Away: %.2f   (%.2f)
+
+    """)
+
+    all_predictions = json.loads(all_predictions_json)
+    mail_message = ""
+
+    for league in all_predictions:
+        mail_message += league_template % (league['div'], league['league'],
+                                           league['home_fname'], league['home_score'],
+                                           league['away_fname'], league['away_score'],
+                                           league['draw_fname'], league['draw_score'])
+
+        for prediction in league['predictions']:
+            mail_message += match_template % (prediction['home_team'], prediction['away_team'],
+                                              prediction['home_pct'], prediction['home_odds'],
+                                              prediction['draw_pct'], prediction['draw_odds'],
+                                              prediction['away_pct'], prediction['away_odds'])
+
+
+    send_mail("Football predictions (Home/Away)", mail_message)
+
+
+def email_best_predictions(all_predictions_json):
+    league_template = textwrap.dedent("""
+
+    ==================================================
+    %s: %s
+    ==================================================
+    """)
+
+    match_template = textwrap.dedent("""
+    %s v. %s
+    --------------------------------------------------
+    %s: %.2f   (%.2f)
+
+    """)
+
+    cutoff = 65
+    if "PREDICTION_CUTOFF" in os.environ:
+        cutoff = int(os.environ['PREDICTION_CUTOFF'])
+
+    all_predictions = json.loads(all_predictions_json)
+    mail_message = ""
+
+    for div_predictions in all_predictions:
+        div = div_predictions['div']
+        league = div_predictions['league']
+        top_predictions = []
+
+        for prediction in div_predictions['predictions']:
+            home_team, away_team = prediction['home_team'], prediction['away_team']
+            if prediction['home_pct'] >= cutoff:
+                top_predictions.append({'home': home_team, 'away': away_team, 'winner': home_team, 'pct': prediction['home_pct'], 'odds': prediction['home_odds']})
+            if prediction['away_pct'] >= cutoff:
+                top_predictions.append({'home': home_team, 'away': away_team, 'winner': away_team, 'pct': prediction['away_pct'], 'odds': prediction['away_odds']})
+
+        if len(top_predictions) > 0:
+            mail_message += league_template % (div, league)
+            for p in top_predictions:
+                mail_message += match_template % (p['home'], p['away'], p['winner'], p['pct'], p['odds'])
+
+    if mail_message:
+        send_mail("Top football predictions (cutoff = %.2f%%)" % cutoff, mail_message)
+
+
+def email_draw_predictions(all_predictions_json):
+    league_template = textwrap.dedent("""
+
+    ==================================================
+    %s: %s
+    ==================================================
+    """)
+
+    match_template = textwrap.dedent("""
+    %s v. %s
+    --------------------------------------------------
+    Draw: %.2f   (%.2f)
+
+    """)
+
+    cutoff = 30
+    if 'DRAW_CUTOFF' in os.environ:
+        cutoff = int(os.environ['DRAW_CUTOFF'])
+
+    all_predictions = json.loads(all_predictions_json)
+    mail_message = ""
+
+    for div_predictions in all_predictions:
+        div = div_predictions['div']
+        league = div_predictions['league']
+        draw_predictions = []
+
+        for prediction in div_predictions['predictions']:
+            home_team, away_team = prediction['home_team'], prediction['away_team']
+            if prediction['draw_pct'] >= cutoff:
+                draw_predictions.append({'home': home_team, 'away': away_team, 'pct': prediction['draw_pct'], 'odds': prediction['draw_odds']})
+
+        if len(draw_predictions) > 0:
+            mail_message += league_template % (div, league)
+            for p in draw_predictions:
+                mail_message += match_template % (p['home'], p['away'], p['pct'], p['odds'])
+
+    if mail_message:
+        send_mail("Football draw predictions (cutoff = %.2f%%)" % cutoff, mail_message)
+
+
+def send_mail(subject, mail_message):
+    LOGGER.info("Sending mail (subject: %s)...", subject)
 
     response = ses.send_email(
         Destination={
@@ -101,13 +241,14 @@ def make_predictions(event):
             },
             'Subject': {
                 'Charset': 'UTF-8',
-                'Data': "Football predictions for {}".format(datetime.now()),
+                'Data': subject,
             }
         },
         Source=from_email_address
     )
 
     LOGGER.info("Mail sent: %s", response)
+
 
 
 def get_historical_data_from_s3(bucket, league):
@@ -160,22 +301,9 @@ def train_classifiers(data, league):
     best_draw_score = draw_scores[draw_best_fname]
     best_away_score = away_scores[away_best_fname]
 
-    training_summary = """
-    ==================================================
-    League: %s
-    ==================================================
-    Home (%s) %.2f
-    Away (%s) %.2f
-    Draw (%s) %.2f
-
-    """ % (league, home_best_fname, best_home_score, away_best_fname, best_away_score, draw_best_fname, best_draw_score)
-    training_summary = textwrap.dedent(training_summary)
-    print(training_summary)
-
     return home_clf, draw_clf, away_clf, \
         best_home_score, best_draw_score, best_away_score,\
-        home_best_fname, draw_best_fname, away_best_fname,\
-        training_summary
+        home_best_fname, draw_best_fname, away_best_fname
 
 
 def get_league_data(data, league):
@@ -379,14 +507,16 @@ def get_latest_away_ppg_for_team(data, div, team):
     return get_latest_home_or_away_ppg_for_team(data, div, team, 'AwayTeam')
 
 
-def predict(data, league, classifiers, matches, csv=False):
+def predict(data, league, classifiers, matches):
     league_data = get_league_data(data, league)
     subset_rows = list(range(len(league_data) - 200, len(league_data)))
     league_data = league_data.subset(rows=subset_rows)
 
     output = ""
+    predictions = []
 
     for match in matches:
+
         home_team, away_team = match
         home_team = common.get_team_name(home_team, league)
         away_team = common.get_team_name(away_team, league)
@@ -403,24 +533,31 @@ def predict(data, league, classifiers, matches, csv=False):
         away_diff = diff_overall_ppg if classifiers['away_fname'] == 'PpgDiff' else diff_home_away_ppg
         draw_diff = diff_overall_ppg if classifiers['draw_fname'] == 'PpgDiff' else diff_home_away_ppg
 
-        home = classifiers['home_clf'].predict([[home_diff]])
-        away = classifiers['away_clf'].predict([[away_diff]])
-        draw = classifiers['draw_clf'].predict([[draw_diff]])
-        if csv:
-            output += "%s,%s,%s,%.2f,%.2f,%.2f" % (league, home_team, away_team, home, draw, away)
-        else:
-            prediction = """
-            %s: %s
-            --------------------------------------------------
-            Home: %.2f   (%.2f)
-            Draw: %.2f   (%.2f)
-            Away: %.2f   (%.2f)
+        home = classifiers['home_clf'].predict([[home_diff]])[0]
+        away = classifiers['away_clf'].predict([[away_diff]])[0]
+        draw = classifiers['draw_clf'].predict([[draw_diff]])[0]
 
-            """ % (league, match, home, 100/home, draw, 100/draw, away, 100/away)
-            output += textwrap.dedent(prediction)
+        prediction = dict()
+        prediction['home_team'] = home_team
+        prediction['away_team'] = away_team
+        prediction['home_pct'] = home
+        prediction['home_odds'] = 1/home*100
+        prediction['draw_pct'] = draw
+        prediction['draw_odds'] = 1/draw*100
+        prediction['away_pct'] = away
+        prediction['away_odds'] = 1/away*100
 
-    output += "\n"
-    print(output)
+        predictions.append(prediction)
 
-    return output
+        output = """
+        %s: %s
+        --------------------------------------------------
+        Home: %.2f   (%.2f)
+        Draw: %.2f   (%.2f)
+        Away: %.2f   (%.2f)
 
+        """ % (league, match, home, 100/home, draw, 100/draw, away, 100/away)
+        output = textwrap.dedent(output)
+        print(output, "\n")
+
+    return predictions
